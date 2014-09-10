039b888364e5febbd7add8b64a0c4b91251dd7fa
Greg Kepler <gkepler@gmail.com>
2014-09-09 23:22:33 -0400

    More messing with cinder Path2d

--- cinder/Path2DSamples/src/Path2DSamplesApp.cpp
+++ cinder/Path2DSamples/src/Path2DSamplesApp.cpp
@@ -66,6 +66,8 @@ void Path2DSamplesApp::update()
 
 void Path2DSamplesApp::draw()
 {
+	gl::enableAlphaBlending();
+	
 	gl::clear( Color( 0, 0, 0 ) );
 		
 	{
@@ -88,13 +90,13 @@ void Path2DSamplesApp::draw()
 	
 	{
 		gl::ScopedMatrices mtrx;
-		gl::translate( vec2( 150.0, 300.0 ) );
+		gl::translate( vec2( 200.0, 300.0 ) );
 		drawPath( path4 );
 	}
 	
 	{
 		gl::ScopedMatrices mtrx;
-		gl::translate( vec2( 250.0, 300.0 ) );
+		gl::translate( vec2( 350.0, 300.0 ) );
 		drawPath( path5 );
 	}
 	// draw outlines
@@ -113,6 +115,9 @@ void Path2DSamplesApp::drawPath( const cinder::Path2d &path )
 	for( int i = 0; i < path.getPoints().size(); i++ ) {
 		vec2 point = path.getPoint( i );
 		gl::drawSolidRect( Rectf( -2.0, -2.0, 2.0, 2.0 ) + point );
+		
+//		gl::drawLine( path.getPosition( t ), mPath.getPosition( t ) + normalize( mPath.getTangent( t ) ) * 80.0f );
+		
 	}
 	
 	// draw point along the curve
@@ -124,20 +129,86 @@ void Path2DSamplesApp::drawPath( const cinder::Path2d &path )
 	gl::drawSolidCircle( pt, 4.0 );
 	
 	//
-	gl::color( 0, 1, 1 );
-	pt = path.getTangent( pos );
-	gl::drawSolidCircle( pt, 4.0 );
+//	gl::color( 0, 1, 1 );
+//	pt = path.getTangent( pos );
+//	gl::drawSolidCircle( pt, 4.0 );
 	
-	/*//draw lines between points depending on their segment type
+	
+	//draw lines between points depending on their segment type
 	gl::color( 1, 0, 1 );
-	console() << " --- " << endl;
+	int ptCount = 0;
 	for( int i = 0; i < path.getSegments().size(); i++ ) {
 		auto segment = path.getSegments()[i];
+//		console() << " --- " << segment << " --- "<< endl;
+		auto points = path.getPoints();
+		
+		// if segment type == Path2d::SegmentType::CUBICTO
+		// draw lines between
+		if( segment == Path2d::SegmentType::QUADTO ) {
+			vec2 pt1 = path.getPoints()[ptCount];
+			vec2 pt2 = path.getPoints()[ptCount + 1];
+			vec2 pt3 = path.getPoints()[ptCount + 2];
+			if( segment == Path2d::SegmentType::QUADTO ) {
+				gl::drawLine( pt1, pt2 );
+				gl::drawLine( pt2, pt3 );
+			}
+		}
+		
+		if( segment == Path2d::SegmentType::CUBICTO ) {
+			vec2 pt1 = path.getPoints()[ptCount + 0];
+			vec2 pt2 = path.getPoints()[ptCount + 1];
+			vec2 pt3 = path.getPoints()[ptCount + 2];
+			vec2 pt4 = path.getPoints()[ptCount + 3];
+			gl::drawLine( pt1, pt2 );
+			gl::drawLine( pt3, pt4 );
+		}
+		
+		
 //		path.getSegmentTangent(size_t segment, float t)
-		console() << segment << endl;
+//		console() << segment << endl;
 //		vec2 point = path.getPoint( i );
 //		gl::drawSolidRect( Rectf( -2.0, -2.0, 2.0, 2.0 ) + point );
-	}*/
+		
+		if( segment == Path2d::SegmentType::MOVETO || segment == Path2d::SegmentType::LINETO )
+			ptCount += 1;
+		if( segment == Path2d::SegmentType::CUBICTO )
+			ptCount += 3;
+		if( segment == Path2d::SegmentType::QUADTO )
+			ptCount += 2;
+	}
+	
+	
+	gl::color( 1, 1, 1, 0.2 );
+//	gl::drawSolidRect( path.calcBoundingBox() );			// box around everything (including points)
+	gl::drawSolidRect( path.calcPreciseBoundingBox() );		// box around just the path itself
+	
+	// calculate length of path
+	gl::drawString( "length: " + to_string( path.calcLength() ), vec2( 0.0, -10.0 ) );
+	
+	// not sure what these do
+	/*static int		calcQuadraticBezierMonotoneRegions( const vec2 p[3], float resultT[2] );
+	static vec2	calcQuadraticBezierPos( const vec2 p[3], float t );
+	static vec2	calcQuadraticBezierDerivative( const vec2 p[3], float t );
+	static int		calcCubicBezierMonotoneRegions( const vec2 p[4], float resultT[4] );
+	static vec2	calcCubicBezierPos( const vec2 p[4], float t );
+	static vec2	calcCubicBezierDerivative( const vec2 p[4], float t );*/
+	
+	{
+		gl::color( 0, 0, 0 );
+		float time = path.calcNormalizedTime( getElapsedSeconds() * 0.25 );		// should this be used when getting position based on time?
+//		float time = path.calcTimeForDistance( getElapsedSeconds() * 0.25 );	// is this based on the actual curve? No. It's based on distance of the current part of the arc or something?
+		vec2 pos = path.getPosition( time );
+		gl::drawSolidCircle( pos, 2.0 );
+	}
+	
+	//
+	//path.calcSegmentLength( segment number, min, max )
+	// highlight  0.25 - 0.75 or each segment
+	
+	// position based on teh actual curve?
+//	path.calcTimeForDistance(float distance)
+	
+	
 }
 
 CINDER_APP_NATIVE( Path2DSamplesApp, RendererGl )
