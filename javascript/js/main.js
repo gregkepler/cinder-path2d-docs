96653658ee9d6ad4bc9cfd862d9b28d61211e19a
Greg Kepler <gkepler@barbariangroup.com>
2014-12-07 11:12:42 -0500

    Moved all of the sketches into their own classes.

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -9,22 +9,50 @@
 // console.log(paper);
 	
 
-	// +———————————————————————————————————————+
-	// 	Path2dSketch     
-	// 	all sketches should extend this
-	// +———————————————————————————————————————+
-/*
-	var Path2dSketch = function( _canvas, _output ) {
 
-		this.canvas = _canvas;
-		this.output = _output;
+
+	//
+	// from http://codepen.io/MyHatIsAwesome/pen/DaptL
+	// Function.extend( base )
+	// sets up inheritance between two constructor functions
+	// without needing to instantiate the base constructor
+	//
+	Function.prototype.extend = function( base )
+	{
+		// Define a surrogate constructor for base
+		var Temp = function(){};
+		Temp.prototype = base.prototype;
+
+		// instantiate the surrogate
+		var t = new Temp();
+		t.constructor = this;
+
+		// copy subclass prototype into surrogate object
+		for( var key in this.prototype )
+			t[key] = this.prototype[key];
+
+		// set subclass prototype as the surrogate object
+		this.prototype = t;
+
+		this.prototype.superclass = base.prototype;
+	};
+
+
+	// +———————————————————————————————————————+
+	//	Path2dSketch     
+	//	all sketches should extend this
+	// +———————————————————————————————————————+
+
+	function Path2dSketch( options ) {
+
+		this.canvas = null;
+		this.output = null;
 		this.paths = [];
-		this.curPaper;
-		this.tool;
-		this.selectedSegment;
-		this.selectedPath;
-		this.selectedPoint;
-
+		this.curPaper = null;
+		this.tool = null;
+		this.selectedSegment = null;
+		this.selectedPath = null;
+		this.selectedPoint = null;
 		this.hitOptions = {
 			segments: true,
 			stroke: true,
@@ -32,28 +60,38 @@
 			tolerance: 5
 		};
 
-		function init(  ){
+		this.initialize( options );
+	}
 
-			this.curPaper = new paper.PaperScope();
-			this.curPaper.setup( this.canvas );
-			log( this );
-			this.papers.push( this.curPaper );
+	Path2dSketch.prototype = {
 
-			this.tool = new curPaper.Tool();
-			this.tool.onMouseMove = onToolMouseMove;
-			this.tool.onMouseDown = onToolMouseDown;
-			// this.output.update( this.path );
-		}
+		initialize: function( opts ) {
 
-		function onToolMouseMove( event ) {
+
+			var options = opts || {};
+			console.log( "PATH2DSKETCH INITIALIZE: output  ", options.output );
+
+			// console.log( "options", options );
+			// use defined options
+			this.canvas = options.canvas;
+			this.output = options.output;
+
+			var paperScope = new paper.PaperScope();
+			paperScope.setup( $( options.canvas )[0]);
+			this.curPaper = paperScope;
+			// console.log( "paths", this, this.paths, paperScope );
+			// this.paths.push( paperScope );
+		},
+
+		onToolMouseMove: function( event ) {
 
 			curPaper.project.activeLayer.selected = false;
 			if (event.item) {
 				event.item.selected = true;
 			}
-		}
+		},
 
-		function onToolMouseDown( event ) {
+		onToolMouseDown: function( event ) {
 
 			selectedSegment = selectedPath = null;
 			var hitResult = curPaper.project.hitTest( event.point, hitOptions );
@@ -72,204 +110,31 @@
 			// if( movePath ) {
 				// project.activeLayer.addChild( hitResult.item );
 			// }
-		}
+		},
 
-		function onToolMouseDrag( event ) {
+		onToolMouseDrag: function( event ) {
 			
 			if( selectedSegment ) {
 				segment.point = event.point;
 			}
 
 			// if( movePath ) {
-			// 	path.position += event.delta;
+			//	path.position += event.delta;
 			// }
 
 			this.output.update( path );
-		}
+		},
 
-		init.apply( this );
-		log( "this", this );
-	}
+		updatePath: function() {
 
-	Path2dSketch.prototype.updatePath = function() {
-
-		this.curPaper.draw();
-		this.output.update( path );
-	}
-
-	Path2dSketch.prototype.extend = function( classVars ) {
-
-		classVars || (classVars = {});
-
-		var obj = function(options) {
-
-			this.initialize && this.initialize();
-		};
-
-		obj.prototype = Object.create( Path2dSketch.prototype );
-		obj.prototype = _.extend(obj.prototype, classVars);
-		obj.prototype.constructor = obj;
-
-		return obj;
-	}
-*/
-
-	/*window.Path2dSketch = function(){
-
-		this.canvas = null;
-		this.output = null;
-		this.paths = [];
-		this.curPaper = null;
-		this.tool = null;
-		this.selectedSegment = null;
-		this.selectedPath = null;
-		this.selectedPoint = null;
-
-	};*/
-
-
-	// extending classes
-	/*function surrogateCtor() {}
-	function extend(base, sub) {
-		surrogateCtor.prototype = base.prototype;
-		sub.prototype = new surrogateCtor();
-		sub.prototype.constructor = sub;
-		sub.superclass = base;
-	}*/
-
-	//
-	// from http://codepen.io/MyHatIsAwesome/pen/DaptL
-	// Function.extend( base )
-	// sets up inheritance between two constructor functions
-	// without needing to instantiate the base constructor
-	//
-	Function.prototype.extend = function( base )
-	{
-	  // Define a surrogate constructor for base
-	  var Temp = function(){}
-	  Temp.prototype = base.prototype
-
-	  // instantiate the surrogate
-	  var t = new Temp()
-	  t.constructor = this
-
-	  // copy subclass prototype into surrogate object
-	  for( key in this.prototype )
-	    t[key] = this.prototype[key]
-
-	  // set subclass prototype as the surrogate object
-	  this.prototype = t
-
-	  // this.prototype.superclass = base;
-	  this.prototype.superclass = base.prototype;
-	  // console.log( "SUPERCLASS", this, this.superclass );
-	}
-
-
-	function Path2dSketch( options ) {
-
-		this.canvas = null;
-		this.output = null;
-		this.paths = [];
-		this.curPaper = null;
-		this.tool = null;
-		this.selectedSegment = null;
-		this.selectedPath = null;
-		this.selectedPoint = null;
-
-		console.log( "Path2dSketch", this.prototype, options );
-		// how to call a function from constructor which doesn't exist yet\
-
-		/*this.initialize = function( opts ) {
-
-			console.log( "Path2dSketch INIT" );
-			var options = opts || {};
-
-			this.canvas = options.canvas;
-			this.output = options.output;
-
-			var paperScope = new paper.PaperScope();
-			paperScope.setup( $( options.canvas )[0]);
-			this.curPaper = paperScope;
-			this.paths.push( paperScope );
-		};*/
-
-		this.initialize( options );
-	}
-
-	Path2dSketch.prototype = {
-
-		initialize: function( opts ) {
-
-			console.log( "PATH2DSKETCH INITIALIZE" );
-
-			var options = opts || {};
-
-			console.log( "options", options );
-			this.canvas = options.canvas;
-			this.output = options.output;
-
-			var paperScope = new paper.PaperScope();
-			paperScope.setup( $( options.canvas )[0]);
-			this.curPaper = paperScope;
-			console.log( "paths", this, this.paths, paperScope );
-			this.paths.push( paperScope );
+			this.curPaper.view.draw();
+			console.log( this.paths[0] );
+			this.output.update( this.paths[0] );
 		}
 	};
 
-	// Path2dSketch.prototype.canvas = null;
-	// Path2dSketch.prototype.paths = [];
-
-	/*window.Path2dSketch = function() {
-
-		return {
-
-			canvas: null,
-			output: null,
-			paths: []
-		};
-
-	};*/
-
-
-
-	// window.Path2dSketch.initialize = function( opts ) {
-	/*Path2dSketch.prototype.initialize = function( opts ) {
-
-		var options = opts || {};
-
-		console.log( "options", options );
-		this.canvas = options.canvas;
-		this.output = options.output;
-
-		var paperScope = new paper.PaperScope();
-		paperScope.setup( $( options.canvas )[0]);
-		this.curPaper = paperScope;
-		console.log( "paths", this, this.paths, paperScope );
-		this.paths.push( paperScope );
-	};*/
-
-	/*// window.Path2dSketch.extend = function( vars ) {
-	Path2dSketch.prototype.extend = function( vars ) {
-
-		var classVars = vars || {};
-
-		var obj = function( options ) {
-
-			this.initialize( options );
-		};
-
-		console.log( "OBJ PROTOTYPE", Path2dSketch.prototype, this );
-		obj.prototype = Object.create( Path2dSketch.prototype );
-		obj.prototype = _.extend( obj.prototype, classVars );
-		obj.prototype.constructor = obj;
-
-		return obj;
-	};*/
-
 
 	
-
 
 	// +———————————————————————————————————————+
 	//	LineToSketch
@@ -282,56 +147,23 @@
 		// LineToSketch.superclass.call( this, options );
 		Path2dSketch.call( this, options );
 
-		
-
 		this.name = "lineto";
-
-		/*this.initialize = function( options ){
-
-			console.log( "LINE TO SKETCH INIT" );
-			// super
-			Path2dSketch.prototype.initialize.call( this, options );
-			// Path2dSketch.call( this, options );
-
-			// draw path
-			this.drawInitialPath();
-		};*/
-
-		/*this.drawInitialPath = function() {
-			console.log( "DRAW INITIAL PATH" );
-
-			// draw the initial path
-			var curPaper = this.curPaper;
-			console.log( "curPaper", curPaper );
-			var path = new curPaper.Path();
-				path.strokeColor = COLOR_LINE_TO;
-				path.moveTo( new curPaper.Point(50.0, 50.0) );
-				path.lineTo( new curPaper.Point(150.0, 150.0) );
-				path.lineTo( new curPaper.Point(250.0, 50.0));
-				path.lineTo( new curPaper.Point(350.0, 150.0) );
-				path.lineTo( new curPaper.Point(450.0, 50.0) );
-			
-		};*/
-		// console.log( " ", this );
-		// this.initialize();
 		
 	}
-	// this needs to come before new prototype functions are added
-	// extend( Path2dSketch, LineToSketch );
-	
 
 	LineToSketch.prototype = {
 		initialize: function( options ) {
-		console.log( "LINE TO SKETCH INIT: superclass", this, this.superclass );
-		this.superclass.initialize.call( this, options );
-		this.drawInitialPath();
-	},
-	drawInitialPath: function( ) {
+			console.log( "LINE TO SKETCH INIT: superclass", this, this.superclass );
+			this.superclass.initialize.call( this, options );
+			this.drawInitialPath();
+			this.updatePath();
+		},
+		drawInitialPath: function( ) {
 			console.log( "DRAW INITIAL PATH" );
 
 			// draw the initial path
 			var curPaper = this.curPaper;
-			console.log( "curPaper", curPaper );
+			console.log( "curPaper", this.curPaper );
 			var path = new curPaper.Path();
 				path.strokeColor = COLOR_LINE_TO;
 				path.moveTo( new curPaper.Point(50.0, 50.0) );
@@ -339,9 +171,92 @@
 				path.lineTo( new curPaper.Point(250.0, 50.0));
 				path.lineTo( new curPaper.Point(350.0, 150.0) );
 				path.lineTo( new curPaper.Point(450.0, 50.0) );
+			this.paths.push( path );
 		}
 	};
 	LineToSketch.extend( Path2dSketch );
+
+
+	// +———————————————————————————————————————+
+	//	QuadToSketch
+	// +———————————————————————————————————————+
+
+	function QuadToSketch( options ) {
+
+		console.log( "QUAD TO SKETCH", Path2dSketch.prototype );
+		Path2dSketch.call( this, options );
+
+		this.name = "quadto";
+	}
+
+	QuadToSketch.prototype = {
+
+		initialize: function( options ) {
+
+			this.superclass.initialize.call( this, options );
+			this.drawInitialPath();
+			// this.updatePath();
+		},
+
+		drawInitialPath: function( ) {
+			
+			// quadTo - waves
+			var curPaper = this.curPaper;
+			var waveWidth = 100.0;
+			var path = new curPaper.Path();
+			path.moveTo( new curPaper.Point(0.0, 50.0) );
+			path.strokeColor = COLOR_QUAD_TO;
+			for( var i = 0; i < 5; i++ ) {
+				var startX = i * waveWidth;
+				path.quadraticCurveTo( new curPaper.Point( startX, 0.0 ), new curPaper.Point( startX + waveWidth / 2.0, 0.0 ) );
+				path.quadraticCurveTo( new curPaper.Point( startX + waveWidth / 2.0, 50.0 ), new curPaper.Point( startX + waveWidth, 50.0 ) );
+
+			}
+			this.paths.push( path );
+		}
+	};
+	QuadToSketch.extend( Path2dSketch );
+
+
+	// +———————————————————————————————————————+
+	//	CubicToSketch
+	// +———————————————————————————————————————+
+
+	function CubicToSketch( options ) {
+
+		console.log( "CUBIC TO SKETCH", Path2dSketch.prototype );
+		Path2dSketch.call( this, options );
+
+		this.name = "cubicto";
+	}
+
+	CubicToSketch.prototype = {
+
+		initialize: function( options ) {
+
+			this.superclass.initialize.call( this, options );
+			this.drawInitialPath();
+			// this.updatePath();
+		},
+
+		drawInitialPath: function( ) {
+			
+			// quadTo - waves
+			var curPaper = this.curPaper;
+			var waveWidth = 100.0;
+			var path = new curPaper.Path();
+			path.moveTo( new curPaper.Point(0.0, 50.0) );
+			path.strokeColor = COLOR_CUBIC_TO;
+			for( var i = 0; i < 5; i++ ) {
+				var startX = i * waveWidth;
+				path.quadraticCurveTo( new curPaper.Point( startX, 0.0 ), new curPaper.Point( startX + waveWidth / 2.0, 0.0 ) );
+				path.quadraticCurveTo( new curPaper.Point( startX + waveWidth / 2.0, 50.0 ), new curPaper.Point( startX + waveWidth, 50.0 ) );
+
+			}
+			this.paths.push( path );
+		}
+	};
+	CubicToSketch.extend( Path2dSketch );
 
 
 
@@ -362,172 +277,12 @@
 		init: function(){
 			
 			// init all the sketches
-			var lineToSketch = new LineToSketch( { canvas:"#lineto", output:"#output"} );
+			var lineToSketch	= new LineToSketch( { canvas:"#lineto", output:window.app.output } );
+			var quadToSketch	= new QuadToSketch( { canvas:"#quadto", output:window.app.output } );
+			var cubicToSketch	= new CubicToSketch( { canvas:"#cubicto", output:window.app.output } );
 			// console.log("lineToSketch", lineToSketch.canvas );
 			// sketches.push( lineToSketch );
 		
-		},
-
-		lineto: {
-			
-			canvas: null,
-			paperScope: null,
-			tool: null,
-			init: function(){
-
-				var curPaper = new paper.PaperScope();
-				curPaper.setup($('#lineto')[0]);
-				papers.push( curPaper );
-
-				// draw the initial path
-				var path = new curPaper.Path();
-					path.strokeColor = COLOR_LINE_TO;
-					path.moveTo( new curPaper.Point(50.0, 50.0) );
-					path.lineTo( new curPaper.Point(150.0, 150.0) );
-					path.lineTo( new curPaper.Point(250.0, 50.0));
-					path.lineTo( new curPaper.Point(350.0, 150.0) );
-					path.lineTo( new curPaper.Point(450.0, 50.0) );
-				curPaper.view.draw();
-				
-				tool = new curPaper.Tool();
-				tool.onMouseMove = function(event) {
-					curPaper.project.activeLayer.selected = false;
-					// log("item", event);
-					if (event.item) {
-						// log("select item");
-						event.item.selected = true;
-
-					}
-				};
-				
-				var segment;
-
-				var hitOptions = {
-					segments: true,
-					stroke: true,
-					fill: true,
-					tolerance: 5
-				};
-
-				tool.onMouseDown = function(event) {
-					segment = path = null;
-					var hitResult = curPaper.project.hitTest(event.point, hitOptions);
-					if (!hitResult)
-						return;
-
-					if (hitResult) {
-						path = hitResult.item;
-						if (hitResult.type == 'segment') {
-							segment = hitResult.segment;
-							console.log("SEGMENT");
-						}
-					}
-					movePath = hitResult.type == 'fill';
-					if (movePath)
-						project.activeLayer.addChild(hitResult.item);
-				};
-
-				tool.onMouseDrag = function(event) {
-
-					log( "drag", segment, movePath );
-					if (segment) {
-						segment.point = event.point;
-					}
-
-					if (movePath){
-						path.position += event.delta;
-					}
-
-					app.output.update( path );
-				};
-
-				app.output.update( path );
-			}
-		},
-
-		quadto: {
-
-			div: null,
-
-			canvas: null,
-
-			init: function(){
-				
-				var curPaper = new paper.PaperScope();
-				curPaper.setup($('#quadto')[0]);
-				papers.push( curPaper );
-
-				
-				var path = new curPaper.Path();
-
-				/*path.strokeColor = '#ff0000';
-				path.moveTo( new curPaper.Point(50.0, 50.0) );
-				path.lineTo( new curPaper.Point(150.0, 150.0) );
-				path.lineTo( new curPaper.Point(250.0, 50.0));
-				path.lineTo( new curPaper.Point(350.0, 150.0) );
-				path.lineTo( new curPaper.Point(450.0, 50.0) );
-				curPaper.view.draw();*/
-
-
-				// quadTo - waves
-				var waveWidth = 100.0;
-				path.moveTo( new curPaper.Point(0.0, 50.0) );
-				path.strokeColor = COLOR_QUAD_TO;
-				for( var i = 0; i < 5; i++ ) {
-					var startX = i * waveWidth;
-					path.quadraticCurveTo( new curPaper.Point( startX, 0.0 ), new curPaper.Point( startX + waveWidth / 2.0, 0.0 ) );
-					path.quadraticCurveTo( new curPaper.Point( startX + waveWidth / 2.0, 50.0 ), new curPaper.Point( startX + waveWidth, 50.0 ) );
-
-				}
-				curPaper.view.draw();
-			}
-		},
-
-		cubicto: {
-
-			sketch: null,
-
-			Sketch: function(){
-
-				this.drawPath = function(){
-					log( "DRAW CUBIC TO");
-				};
-			},
-
-			init: function() {
-
-				// extend( app.cubicto.Sketch, Path2dSketch );
-				var sketch = new app.cubicto.Sketch( $('#cubicto')[0], output  );
-				
-				// var sketch = _.extend( Path2dSketch, {} );
-				// sketch.init( $('#cubicto')[0] );
-				// log( "extended sketch", sketch );
-				// var sketch = new Path2dSketch( $('#cubicto')[0], output );
-
-				// sketch.drawInitialPath = function(){
-
-				// 	console.log( this );
-				// 	var path = new this.curPaper.Path();
-				// 		path.strokeColor = COLOR_LINE_TO;
-				// 		path.moveTo( new curPaper.Point(50.0, 50.0) );
-				// 		path.lineTo( new curPaper.Point(150.0, 150.0) );
-				// 		path.lineTo( new curPaper.Point(250.0, 50.0));
-				// 		path.lineTo( new curPaper.Point(350.0, 150.0) );
-				// 		path.lineTo( new curPaper.Point(450.0, 50.0) );
-				// 	this.curPaper.view.draw();
-				// 	this.updatePath();
-				// }
-
-				// sketch.drawInitialPath();
-
-				/*sketch.drawInitialPath() {
-					// draw the initial path
-					
-				}*/
-
-			}
-
-
 		},
 
 		output: {
@@ -555,6 +310,8 @@
 					}else{
 						code += "mPath.lineTo( Vec2f( " + segment.point.x + ", " + segment.point.y + " ) );\n";
 					}
+
+					// TODO: base the template for the line segment based on the segment type
 				}
 
 				code += "gl::draw( mPath );";
@@ -595,9 +352,8 @@
 
 		// create the following interactive displays
 		if( $( '#output' ).size() > 0 )		{ window.app.output.init(); }
-		if( $( '#lineto' ).size() > 0 )		{ window.app.lineto.init(); }
-		if( $( '#quadto' ).size() > 0 )		{ window.app.quadto.init(); }
-		if( $( '#cubicto' ).size() > 0 )	{ window.app.cubicto.init(); }
+		// if( $( '#quadto' ).size() > 0 )		{ window.app.quadto.init(); }
+		// if( $( '#cubicto' ).size() > 0 )	{ window.app.cubicto.init(); }
 
 		// ideal syntax
 		// window.app.sketchLineTo( "#lineto" );
